54a51711f7a1b0bd42575a9d18effd40c48e8f49
aek <adil.elkanabi@arrow.com>
2017-05-26 10:54:57 +0200

    Init project with behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -18,6 +18,38 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
-        $this->redis = new Redis();
+
+    }
+
+    /**
+     * @Given there is a :arg1, which costs £:arg2
+     */
+    public function thereIsAWhichCostsPs($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product(s) in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($arg1)
+    {
+        throw new PendingException();
     }
 }
